5dff2a2e2a9682f3e3fd97bd18f30c0aa17a95f1
NestTiga <nestortigasi371@gmail.com>
2024-04-28 22:08:52 -0500

    Validación completa del nombre de usuario

--- src/main/java/com/nestor/login/entity/Usuario.java
+++ src/main/java/com/nestor/login/entity/Usuario.java
@@ -2,6 +2,8 @@ package com.nestor.login.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +14,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "usuarios")
+@Table(
+        name = "usuarios",
+        uniqueConstraints = @UniqueConstraint(
+                name = "usuario_unico",
+                columnNames = "nombreUsuario"
+        )
+        /* con el uniqueConstraints puedo validar que no se ingrese
+        nombres de usuarios duplicados
+         */
+)
 public class Usuario {
 
     @Id
@@ -25,6 +36,10 @@ public class Usuario {
             strategy = GenerationType.SEQUENCE
     )
     private Long ususarioId;
+    @Pattern(regexp = "^[A-Za-z0-9]*$", message = "El nombre de usuario no debe contener signos")
+    @Pattern(regexp = ".*\\d+.*", message = "El nombre de usuario debe contener al menos un número")
+    @Pattern(regexp = ".*[A-Z]+.*", message = "El nombre de usuario debe contener al menos una letra mayúscula")
+    @Size(min = 8, max = 20, message = "El nombre de usuario debe tener entre 8 y 20 caracteres")
     private String nombreUsuario;
     private String contrasenia;
 
